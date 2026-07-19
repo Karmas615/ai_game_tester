3e6379c9285c3aedf07382d2e8c3b762397c474f
Karmas615 <johnl8125@gmail.com>
2026-07-19 17:05:13 -0400

    Day 9 MSS

--- src/capture.py
+++ src/capture.py
@@ -1,2 +1,22 @@
-def capture_screen():
-    return "Screen captured!"
+import mss
+import numpy as np
+import cv2
+import time
+import os
+
+def capture_screen(save=False):
+    with mss.mss() as sct:
+        monitor = sct.monitors[1]  # full screen
+        frame = sct.grab(monitor)
+
+        # Convert BGRA → BGR
+        img = np.array(frame)
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+
+        # Auto-save screenshot if enabled
+        if save:
+            os.makedirs("screenshots", exist_ok=True)
+            filename = f"screenshots/{int(time.time())}.png"
+            cv2.imwrite(filename, img)
+
+        return img
--- src/main.py
+++ src/main.py
@@ -4,10 +4,14 @@ from ocr import read_text
 from automation import click_button
 from state_machine import get_state
 from reporter import report_issue
+import cv2 as cv2
 
 def main():
     print("AI Game Tester starting...")
-    print(capture_screen())
+    frame = capture_screen()
+    cv2.imshow("test", frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     print(detect_ui())
     print(read_text())
     print(click_button())
